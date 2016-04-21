26507e31fed6b18a5bdb72d744e502bcdb8b7aef
Paul Oster <paulo@visionsystems.tv>
2016-04-21 14:55:39 -0500

    added quotachecker

--- quota/quotaGraph.php
+++ quota/quotaGraph.php
@@ -8,6 +8,12 @@ if(!isset($_GET['use'])) {
 	header("Location: quota.php");
 	exit();
 }
+if(isset($_GET['dom'])) {
+	$dom=$_GET['dom'];
+} else {
+	$dom=date('j');
+}
+$total_days = date('t');
 
 $quota=$_GET['quota'];
 $use  =$_GET['use'];
@@ -24,21 +30,23 @@ $white	= imagecolorallocate($im,255,255,255);
 $black	= imagecolorallocate($im,0,0,0);
 imagefilledrectangle($im,0,0,WIDTH,HEIGHT,$white);
 $pct=$use/$quota;
-if($pct > .9) {
+$pct_red	=.9*$dom/$total_days;
+$pct_yellow	=.75*$dom/$total_days;
+if($pct > $pct_red) {
 	if($pct > 1) {
 		$edge=WIDTH;
 	} else {
 		$edge=ceil(WIDTH*$pct);
 	}
 	imagefilledrectangle($im,0,0,$edge,HEIGHT,$red);
-	$edge=ceil(WIDTH*.9);
+	$edge=ceil(WIDTH*$pct_red);
 	imagefilledrectangle($im,0,0,$edge,HEIGHT,$yellow);
-	$edge=ceil(WIDTH*.75);
+	$edge=ceil(WIDTH*$pct_yellow);
 	imagefilledrectangle($im,0,0,$edge,HEIGHT,$green);
-} elseif($pct >=.75) {
+} elseif($pct >= $pct_yellow) {
 	$edge=ceil(WIDTH*$pct);
 	imagefilledrectangle($im,0,0,$edge,HEIGHT,$yellow);
-	$edge=ceil(WIDTH*.75);
+	$edge=ceil(WIDTH*$pct_yellow);
 	imagefilledrectangle($im,0,0,$edge,HEIGHT,$green);
 } else {
 	$edge=ceil(WIDTH*$pct);
@@ -48,10 +56,10 @@ if($pct > .9) {
 
 
 header("Content-type: image/png");
-// Yello Start
-imagefilledrectangle($im,ceil(WIDTH*.75),0,ceil(WIDTH*.75)+3,HEIGHT-1,$yellow);
+// Yellow Start
+imagefilledrectangle($im,ceil(WIDTH*$pct_yellow),0,ceil(WIDTH*$pct_yellow)+3,HEIGHT-1,$yellow);
 // Red Start
-imagefilledrectangle($im,ceil(WIDTH*.9),0,ceil(WIDTH*.9)+3,HEIGHT-1,$red);
+imagefilledrectangle($im,ceil(WIDTH*$pct_red),0,ceil(WIDTH*$pct_red)+3,HEIGHT-1,$red);
 imagerectangle($im,0,0,WIDTH-1,HEIGHT-1,$black);
 imagestring($im,3,25,5,"{$use} GB / {$quota} GB Transfer Used",$black);
 imagepng($im);
